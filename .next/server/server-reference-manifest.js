1785b50a2800e5806741cb0fac86f30bd2cca82b
Grigorev Viktor <4342718@gmail.com>
2025-08-14 21:56:52 +0300

    doc: update readme.md

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Akq7cWr6HuQKgHFE64oxqMbKQCiLNFar/zGSnjmN/Kc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
